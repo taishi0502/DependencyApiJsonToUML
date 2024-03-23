e4324a125a17250179eef3d58aad1f022f90bac0
taishi0502 <kimura.taishi@robotpayment.co.jp>
2024-03-23 22:36:37 +0900

    first commit

---
+++ change.php
@@ -0,0 +1,36 @@
+<?php
+
+// コマンドライン引数からJSONファイルを読み込む
+$jsonFile = $argv[1];
+$outputFile = "output.md";
+
+// ファイル名が重複している場合には、末尾の数字を増やしていく
+$counter = 1;
+while (file_exists($outputFile)) {
+    // ファイル名の末尾に数字を追加
+    $outputFile = "output_" . $counter . ".md";
+    $counter++;
+}
+
+// ファイルを読み込んでデコード
+$jsonData = json_decode(file_get_contents($jsonFile), true);
+
+// 出力用の配列を準備
+$markupList = ["```mermaid\n\nclassDiagram". "\n\n"];
+
+// データを解析してmarkupListに追加
+foreach ($jsonData['records'] as $member) {
+    $compName = $member['MetadataComponentName'];
+    $refName = $member['RefMetadataComponentName'];
+    $markupList[] = "    class ". $compName."{\n\n    }"."\n".
+                    "    class ". $refName."{\n\n    }"."\n"
+                    ."    ".$compName . " --> " . $refName . " : " . "\n\n";
+}
+
+// 出力用の配列を準備
+$markupList[] = "\n```";
+
+// ファイルに書き込み
+file_put_contents($outputFile, implode('', $markupList));
+
+?>
---
+++ output.md
@@ -0,0 +1,22 @@
+```mermaid
+
+classDiagram
+
+    class Test2Batch{
+
+    }
+    class TestLogger{
+
+    }
+    Test2Batch --> TestLogger : 
+
+    class TestBatch{
+
+    }
+    class TestApi{
+
+    }
+    TestBatch --> TestApi : 
+
+
+```
